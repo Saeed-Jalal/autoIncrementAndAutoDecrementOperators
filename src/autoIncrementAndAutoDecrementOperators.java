ed275a24dbd8c19daefa4905623fd23c33b2772a
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-05-30 18:21:43 +0300

    How increment and decrement works in Java

---
+++ src/autoIncrementAndAutoDecrementOperators.java
@@ -0,0 +1,16 @@
+public class autoIncrementAndAutoDecrementOperators {
+    public static void main(String args[]){
+        int num1=100;
+        int num2=225;
+        num1++;
+        //num++ is equivalent to num=num+1;
+        System.out.println("num1++ is:"+num1);
+
+        num2--;
+        //num-- is equivalent to num=num-1;
+        System.out.println("num2-- is:"+num2);
+
+
+
+    }
+}
